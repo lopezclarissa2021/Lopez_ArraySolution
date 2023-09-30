6580b3e68362dd0b54090596e4bedf38b83bb6ea
Clarissa Lopez <102737214+lopezclarissa2021@users.noreply.github.com>
2023-09-30 09:49:48 -0700

    Add project files.

---
+++ Lopez_Array/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Diagnostics;
+
+namespace Lopez_Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            List<string> charList = GetRandomCharacterSet();
+            //string charString = string.Join(",", charList);
+            //Console.WriteLine(charString);
+            sw.Stop();
+            Console.WriteLine($"Operation took {sw.Elapsed.Milliseconds.ToString()} milliseconds to run");
+        }
+
+        public static List<string> GetRandomCharacterSet()
+        {
+            var random = new Random();
+            HashSet<int> numbers = new HashSet<int>();
+            List<string> characters = new List<string>();
+            while (numbers.Count < 50)
+            {
+                int rand = random.Next(65, 122);
+                if (rand > 90 && rand < 97)
+                {
+                    continue;
+                }
+
+                if (numbers.Add(rand))
+                {
+                    characters.Add(Convert.ToChar(rand).ToString());
+                }
+            }
+            return characters;
+        }
+    }
+}
